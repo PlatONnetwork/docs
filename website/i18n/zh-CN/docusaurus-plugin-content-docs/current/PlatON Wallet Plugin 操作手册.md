75bdad6636965b8b498969a137b07a5f07f082a4
lifapan <230469827@qq.com>
2023-06-08 10:47:23 +0800

    add PlatON_Wallet_Plugin_Sdk and PlatON_Wallet_Operation_Manual

---
+++ website/i18n/zh-CN/docusaurus-plugin-content-docs/current/PlatON Wallet Plugin 操作手册.md
@@ -0,0 +1,53 @@
+---
+id: PlatON_Wallet_Operation_Manual
+title: PlatON-Wallet - 操作手册
+sidebar_label: PlatON-Wallet操作手册
+---
+
+# PlatON Wallet 操作手册
+
+## 概述
+  PlatON 钱包插件集成了钱包基础功能、跨链、法币通道、Swap 等多种功能。未来还将实现跨币种的兑换、基于密态支付的交易鉴权、预付费卡销售、虚拟 VISA 卡等更多功能。
+
+  PlatON 钱包插件兼容多种去中心化钱包，应用方通过集成 PlatON 钱包插件后，可以使得其用户方便的进行跨链充值、法币购买等操作。
+
+  为了满足项目方的各种集成需求，我们将为一系列语言和框架提供 SDK，使应用开发人员可以根据不同的业务场景快速完成集成。
+
+## 集成方式
+
+  - [PlatON Wallet Plugin SDK](/docs/zh-CN/PlatON_Wallet_Plugin_Sdk)：提供完整的钱包功能和页面，通过简单的单点集成，可快速使用钱包内所有的功能。
+
+   我们也即将提供更多种集成方式，以满足应用不同场景的集成需求。
+
+## 使用教程 
+> **注意：**由于 PlatON 钱包插件本身不生成账户地址，依赖于其他有账户第三方的钱包，如Metamask等。因此所有的交易将通过第三方钱包来完成。
+
+**1、连接钱包**
+ 当应用集成 PlatON 钱包插件之后，通过应用连接Metamask之后，PlatON钱包也会同步连接上Metamask。连接 Metamask 之后，PlatON 钱包插件将展示该账户在 PlatON 网络上资产情况。
+
+<img src="/docs/img/zh-CN/PlatON-Wallet-imgs/connect.jpg" alt="connect"/>
+
+**2、发送与接收**
+ 钱包插件支持发送和接收 PlatON 网络上的 Token。
+
+<img src="/docs/img/zh-CN/PlatON-Wallet-imgs/send-receive.jpg" alt="send-receive"/>
+
+**3、充值**
+  目前提供两种方式来进行 PlatON 网络资产的充值：
+
+-  方式一：通过跨链桥的方式从其他网络跨入资产到 PlatON 网络
+-  方式二：通过集成 Alchemy Pay，支持以法币支付的方式购买 PlatON 网络上的资产
+  未来我们还将支持更多种便捷的充值方式。
+   
+<img src="/docs/img/zh-CN/PlatON-Wallet-imgs/deposit.jpg" alt="deposit"/>
+
+**4、提取**
+  目前我们有两种方式可将 PlatON 上的资产提取出去：
+
+-  方式一：通过跨链桥的方式将 PlatON 网络资产提取到其他网络。
+- 方式二 ：通过 [txnhub.io](https://txnhub.io) 购买购物卡。
+    
+<img src="/docs/img/zh-CN/PlatON-Wallet-imgs/withdraw.jpg" alt="withdraw"/>
+
+**5、更多功能**
+我们后续将支持更多网络，更多币种的跨链，不同币种之间的Swap，以及基于密态支付的交易鉴权、虚拟 VISA 卡等等更多功能。
--- website/sidebars.js
+++ website/sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       type: 'category',
       label: 'Basics',
-      items: ['lat_introduced', 'staking_and_delegation', 'Network_Description']
+      items: ['lat_introduced', 'staking_and_delegation', 'Network_Description'],
     },
     {
       type: 'category',
@@ -14,8 +14,8 @@ module.exports = {
         'Economic_Model',
         'PlatON_Solution',
         'PlatON_Governance_Solution',
-        'Wasm_Operation_Principle'
-      ]
+        'Wasm_Operation_Principle',
+      ],
     },
     {
       type: 'category',
@@ -26,9 +26,9 @@ module.exports = {
         {
           type: 'category',
           label: 'Tools for nodes',
-          items: ['OnLine_MTool_Manual', 'OffLine_MTool_Manual', 'Command_Line_Tools', 'CTool_Manual']
-        }
-      ]
+          items: ['OnLine_MTool_Manual', 'OffLine_MTool_Manual', 'Command_Line_Tools', 'CTool_Manual'],
+        },
+      ],
     },
     {
       type: 'category',
@@ -38,7 +38,7 @@ module.exports = {
         {
           type: 'category',
           label: 'Set up local environment',
-          items: ['Join_Dev_Network', 'Build_Private_Chain']
+          items: ['Join_Dev_Network', 'Build_Private_Chain'],
         },
         {
           type: 'category',
@@ -47,10 +47,7 @@ module.exports = {
             {
               type: 'category',
               label: 'System contract',
-              items : [
-                'PlatON_system_contract',
-                'PlatON_system_contract_api'
-              ]
+              items: ['PlatON_system_contract', 'PlatON_system_contract_api'],
             },
             {
               type: 'category',
@@ -61,58 +58,78 @@ module.exports = {
                 'Solidity_Contract_Dev_Costs',
                 'Solidity_Contract_Best_Practice',
                 'Solidity_Contract_Security_Dev_Guide',
-                'Solidity_Inner_Contract'
-              ]
+                'Solidity_Inner_Contract',
+              ],
             },
             {
               type: 'category',
               label: 'Wasm contract',
-              items: ['Wasm_Dev_Manual', 'Wasm_Contract_Dev_Costs', 'Wasm_Contract_Best_Practice', 'Wasm_Contract_API']
-            }
-          ]
+              items: ['Wasm_Dev_Manual', 'Wasm_Contract_Dev_Costs', 'Wasm_Contract_Best_Practice', 'Wasm_Contract_API'],
+            },
+          ],
         },
         {
           type: 'category',
           label: 'References',
-          items: ['Python_SDK', 'JS_SDK', 'Java_SDK', 'Go_SDK', 'NET_SDK', 'Json_Rpc','GraphQL_Server', 'Explorer_API', 'Samurai_API']
+          items: [
+            'Python_SDK',
+            'JS_SDK',
+            'Java_SDK',
+            'Go_SDK',
+            'NET_SDK',
+            'Json_Rpc',
+            'GraphQL_Server',
+            'Explorer_API',
+            'Samurai_API',
+          ],
         },
         {
           type: 'category',
           label: 'PRC Token',
-          items: ['PRC20_contract', 'PRC721_contract']
-        }
-      ]
+          items: ['PRC20_contract', 'PRC721_contract'],
+        },
+      ],
     },
     {
       type: 'category',
       label: 'Data and analytics',
-      items: ['PlatON_BlockChain_Browser', 'PlatEye']
+      items: ['PlatON_BlockChain_Browser', 'PlatEye'],
     },
     {
       type: 'category',
       label: 'Tutorials',
       items: [
-         'DApp_migrate', 
-         'qianqian_prc721_tutorial', 
+        'DApp_migrate',
+        'qianqian_prc721_tutorial',
         {
           type: 'category',
           label: 'WASM Tutorials',
-          items: ['WASM_Contract_1', 'WASM_Contract_2', 'WASM_Contract_3', 'WASM_Contract_4', 'WASM_Contract_5', 'WASM_Contract_6', 'WASM_Contract_7', 'WASM_Contract_8', 'WASM_Contract_9']
+          items: [
+            'WASM_Contract_1',
+            'WASM_Contract_2',
+            'WASM_Contract_3',
+            'WASM_Contract_4',
+            'WASM_Contract_5',
+            'WASM_Contract_6',
+            'WASM_Contract_7',
+            'WASM_Contract_8',
+            'WASM_Contract_9',
+          ],
         },
         {
           type: 'category',
           label: 'WalletConnect',
-          items: ['walletconnect_tutorial', 'DApp_integration_with_WalletConnect_for_ATON']
+          items: ['walletconnect_tutorial', 'DApp_integration_with_WalletConnect_for_ATON'],
         },
-		'get_vrf_random_number',
-		'adapting_to_new_chainid'
-        ]
+        'get_vrf_random_number',
+        'eip55-bech32-compatible',
+      ],
     },
     {
       type: 'category',
       label: 'Wallet',
-      items:['ATON-user-manual', 'Samurai_user_manual', 'Third_Party_Walle','Ledger-hardware-wallet', 'MetaMask']
+      items: ['ATON-user-manual', 'Samurai_user_manual', 'Third_Party_Walle', 'Ledger-hardware-wallet', 'MetaMask','PlatON_Wallet_Operation_Manual','PlatON_Wallet_Plugin_Sdk'],
     },
     'community',
-  ]
-};
+  ],
+}
